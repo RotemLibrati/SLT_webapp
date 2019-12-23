4c917e082475b5be8171091831c8cb4fd7819123
omerhajbi <0523ohajbi@gmail.com>
2019-12-23 23:08:20 +0200

    check1

---
+++ SLT_webapp/registration/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.user_name
+
+class Friend(models.Model):
+    users_friend = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Create your models here.
