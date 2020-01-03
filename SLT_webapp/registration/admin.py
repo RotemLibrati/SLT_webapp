2bb4ea3f90d38d9308581e01cc45821156d0bc07
Eilonc285 <eiloncohen285@gmail.com>
2020-01-03 03:14:14 +0200

    game addition

--- SLT_webapp/registration/admin.py
+++ SLT_webapp/registration/admin.py
@@ -4,17 +4,7 @@ from .models import Card
 from .models import GameSession
 
 
-
-class FriendInline(admin.StackedInline):
-    model = Friend
-    extra = 5
-
-
-class UserAdmin(admin.ModelAdmin):
-    inlines = [FriendInline]
-
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.register(Friend)
 admin.site.register(Prize)
 admin.site.register(Winning)
--- SLT_webapp/registration/urls.py
+++ SLT_webapp/registration/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:user_id>/make-new-card/', views.make_new_card, name='make-new-card'),
     path('<int:user_id>/card-check/', views.card_check, name='card-check'),
     path('<int:user_id>/success/', views.success, name='success'),
+    path('<int:user_id>/game/', views.game, name='game'),
 ]
