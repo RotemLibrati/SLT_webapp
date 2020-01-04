68809dd42aac25d113e1f1b2002d4d1bc4d8e564
Eilonc285 <eiloncohen285@gmail.com>
2020-01-04 14:40:44 +0200

    fixed django messages localy by adding django.contrib.sites into installed apps and site_id=1 below it

--- SLT_webapp/SLT_webapp/settings.py
+++ SLT_webapp/SLT_webapp/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_messages'
+    'django.contrib.sites',
+    'django_messages',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
