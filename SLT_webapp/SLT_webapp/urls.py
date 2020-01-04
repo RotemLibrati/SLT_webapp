e963d9715a2ffc39ddaa16538749386c632e3ef7
Eilonc285 <eiloncohen285@gmail.com>
2020-01-04 14:40:44 +0200

    fixed django messages localy by adding django.contrib.sites into installed apps and site_id=1 below it

--- SLT_webapp/SLT_webapp/urls.py
+++ SLT_webapp/SLT_webapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#include('django_messages.urls')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')),
+    path('messages/', include('django_messages.urls')),
 ]
