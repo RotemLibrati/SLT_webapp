c7e8659cc6c5d26a6c808eebb728a87e46ee716c
Eilonc285 <eiloncohen285@gmail.com>
2020-01-05 16:08:47 +0200

    new tests 2

--- SLT_webapp/registration/tests.py
+++ SLT_webapp/registration/tests.py
@@ -31,3 +31,23 @@ class IndexViewTests(TestCase):
         response = self.client.get(reverse('registration:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Sign Language Teacher")
+
+    def test_game_links_without_login(self):
+        response = self.client.get(reverse("registration:index"))
+        self.assertContains(response, 'Login with existing user')
+        self.assertContains(response, 'Make a new user')
+
+
+# class LogInTest(TestCase):
+#     def setUp(self):
+#         self.credentials = {
+#             'username': 'testuser',
+#             'password': 'secret'}
+#         u1 = User(**self.credentials)
+#         u1.save()
+#
+#     def test_login(self):
+#         # send login data
+#         response = self.client.post(reverse('registration:login'), self.credentials, follow=True)
+#         # should be logged in now
+#         self.assertTrue(response.context['user'].is_authenticated)
