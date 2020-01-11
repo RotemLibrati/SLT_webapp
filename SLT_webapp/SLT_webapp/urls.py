369f62848f82276c09c0a32cb5b70ddf2aa5d8e9
Niro-dev <nirb1992@gmail.com>
2020-01-11 17:05:38 +0200

    trying chat (regular) #2

--- SLT_webapp/SLT_webapp/urls.py
+++ SLT_webapp/SLT_webapp/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html")),
-    path('messages/', include('chat.urls')),
     path('admin/', admin.site.urls),
     path('sign-language-teacher/', include('registration.urls')),
 ]
