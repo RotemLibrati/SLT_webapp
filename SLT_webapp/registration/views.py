34360e9231c52cb4921e9ace4b427e148ea00bdc
Niro-dev <nirb1992@gmail.com>
2020-01-14 20:22:26 +0200

    inbox designed add-friend designed

--- SLT_webapp/registration/views.py
+++ SLT_webapp/registration/views.py
@@ -148,7 +148,7 @@ def profile(request):
         friends = list(map(lambda x: x.username, friend.users.all()))
     except (TypeError, Friend.DoesNotExist):
         friends = []
-        messages.add_message(request, messages.INFO, 'Hello world.')
+        # messages.add_message(request, messages.INFO, 'Hello world.')
     up1 = get_object_or_404(UserProfile, user=u1)
     return render(request, 'registration/details.html', {'user': u1, 'profile': up1, 'friends': friends})
 
